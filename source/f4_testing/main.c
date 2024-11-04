f7c99e138fa22fe0d5f0f48c86645a7291b399c2
Eileen Yoon <eyn@gmx.com>
2024-11-04 14:16:07 -0500

    daq: blink led

--- source/f4_testing/main.c
+++ source/f4_testing/main.c
@@ -1,78 +1,74 @@
 #include "common/phal_F4_F7/rcc/rcc.h"
 #include "common/phal_F4_F7/gpio/gpio.h"
-#include "common/phal_F4_F7/adc/adc.h"
-#include "common/phal_F4_F7/dma/dma.h"
-#include "common/phal_F4_F7/spi/spi.h"
-#include "common/phal_F4_F7/usart/usart.h"
 #include "common/psched/psched.h"
-#include "string.h"
+#include "common/phal_F4_F7/spi/spi.h"
 
-#include "main.h"
+#include "common/modules/Wiznet/W5500/Ethernet/W5500/w5500.h"
 
-volatile raw_adc_values_t raw_adc_values;
+#define _W5500_SPI_VDM_OP_          0x00
+#define _W5500_SPI_FDM_OP_LEN1_     0x01
+#define _W5500_SPI_FDM_OP_LEN2_     0x02
+#define _W5500_SPI_FDM_OP_LEN4_     0x03
 
-/* ADC Configuration */
-ADCInitConfig_t adc_config = {
-   .clock_prescaler = ADC_CLK_PRESC_6,
-   .resolution      = ADC_RES_12_BIT,
-   .data_align      = ADC_DATA_ALIGN_RIGHT,
-   .cont_conv_mode  = true,
-   .adc_number      = 1,
-//    .overrun         = true,
-   .dma_mode        = ADC_DMA_CIRCULAR
-};
+volatile uint32_t tick_ms; // Systick 1ms counter
+#define ETH_PHY_RESET_PERIOD_MS 10
+#define ETH_PHY_LINK_TIMEOUT_MS 5000
 
+// SPI1 NSS PA4
+#define DAQ_SPI1_NSS_PORT GPIOA
+#define DAQ_SPI1_NSS_PIN  4
+// SPI1 SCK PA5
+#define DAQ_SPI1_SCK_PORT GPIOA
+#define DAQ_SPI1_SCK_PIN  5
+// SPI1 MISO PA6
+#define DAQ_SPI1_MISO_PORT GPIOA
+#define DAQ_SPI1_MISO_PIN  6
+// SPI1 MOSI PA7
+#define DAQ_SPI1_MOSI_PORT GPIOA
+#define DAQ_SPI1_MOSI_PIN  7
 
-dma_init_t usart_tx_dma_config = USART2_TXDMA_CONT_CONFIG(NULL, 1);
-dma_init_t usart_rx_dma_config = USART2_RXDMA_CONT_CONFIG(NULL, 2);
-usart_init_t lcd = {
-   .baud_rate   = 250000,
-   .word_length = WORD_8,
-   .stop_bits   = SB_ONE,
-   .parity      = PT_NONE,
-   .hw_flow_ctl = HW_DISABLE,
-   .ovsample    = OV_16,
-   .obsample    = OB_DISABLE,
-   .periph      = USART2,
-   .wake_addr = false,
-   .usart_active_num = USART2_ACTIVE_IDX,
-   .tx_dma_cfg = &usart_tx_dma_config,
-   .rx_dma_cfg = &usart_rx_dma_config
-};
-
-#define WHO_AM_I 0x8F
-#define I_AM_HIM 0x3F
-
-// TODO: check prescaler for udpate rate
-ADCChannelConfig_t adc_channel_config[] = {
-   {.channel=0, .rank=1, .sampling_time=ADC_CHN_SMP_CYCLES_480},
-};
-dma_init_t adc_dma_config = ADC1_DMA_CONT_CONFIG((uint32_t) &raw_adc_values, sizeof(raw_adc_values) / sizeof(raw_adc_values.testval), 0b01);
+#define DAQ_ETH_RST_PORT   GPIOE
+#define DAQ_ETH_RST_PIN    3
 
 GPIOInitConfig_t gpio_config[] = {
-    // GPIO_INIT_ANALOG(GPIOA, 0),
+    // DAQ LEDs
+    GPIO_INIT_OUTPUT(GPIOA, 8, GPIO_OUTPUT_LOW_SPEED),
+    GPIO_INIT_OUTPUT(GPIOA, 9, GPIO_OUTPUT_LOW_SPEED),
+    GPIO_INIT_OUTPUT(GPIOA, 10, GPIO_OUTPUT_LOW_SPEED),
     GPIO_INIT_OUTPUT(GPIOD, 13, GPIO_OUTPUT_LOW_SPEED),
-    GPIO_INIT_OUTPUT(GPIOD, 12, GPIO_OUTPUT_LOW_SPEED),
     GPIO_INIT_OUTPUT(GPIOD, 14, GPIO_OUTPUT_LOW_SPEED),
     GPIO_INIT_OUTPUT(GPIOD, 15, GPIO_OUTPUT_LOW_SPEED),
-    GPIO_INIT_USART2TX_PA2,
-    GPIO_INIT_USART2RX_PA3,
-
-    GPIO_INIT_OUTPUT(SPI_CS_PORT, SPI_CS_PIN, GPIO_OUTPUT_HIGH_SPEED),
-    GPIO_INIT_AF(SPI_SCK_PORT, SPI_SCK_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
-    GPIO_INIT_AF(SPI_MOSI_PORT, SPI_MOSI_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
-    GPIO_INIT_AF(SPI_MISO_PORT, SPI_MISO_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN),
-
+#if 1
+    // DAQ W5500 SPI
+    GPIO_INIT_OUTPUT(DAQ_SPI1_NSS_PORT, DAQ_SPI1_NSS_PIN, GPIO_OUTPUT_HIGH_SPEED),
+    GPIO_INIT_AF(DAQ_SPI1_SCK_PORT, DAQ_SPI1_SCK_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_AF(DAQ_SPI1_MISO_PORT, DAQ_SPI1_MISO_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN),
+    GPIO_INIT_AF(DAQ_SPI1_MOSI_PORT, DAQ_SPI1_MOSI_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_OUTPUT_OPEN_DRAIN(DAQ_ETH_RST_PORT, DAQ_ETH_RST_PIN, GPIO_OUTPUT_LOW_SPEED),
+#endif
 };
 
 #define TargetCoreClockrateHz 16000000
 ClockRateConfig_t clock_config = {
     .system_source              =SYSTEM_CLOCK_SRC_HSI,
-    .vco_output_rate_target_hz  =160000000,
+    .vco_output_rate_target_hz  =16000000,
     .system_clock_target_hz     =TargetCoreClockrateHz,
     .ahb_clock_target_hz        =(TargetCoreClockrateHz / 1),
     .apb1_clock_target_hz       =(TargetCoreClockrateHz / (1)),
     .apb2_clock_target_hz       =(TargetCoreClockrateHz / (1)),
+};
+
+dma_init_t spi1_rx_dma_config = SPI1_RXDMA_CONT_CONFIG(NULL, 2);
+dma_init_t spi1_tx_dma_config = SPI1_TXDMA_CONT_CONFIG(NULL, 1);
+
+SPI_InitConfig_t daq_spi1_config = {
+    .data_len  = 8,
+    .nss_sw = false,
+    .nss_gpio_port = DAQ_SPI1_NSS_PORT,
+    .nss_gpio_pin = DAQ_SPI1_NSS_PIN,
+    .rx_dma_cfg = &spi1_rx_dma_config,
+    .tx_dma_cfg = &spi1_tx_dma_config,
+    .periph = SPI1,
 };
 
 extern uint32_t APB1ClockRateHz;
@@ -80,35 +76,20 @@ extern uint32_t APB2ClockRateHz;
 extern uint32_t AHBClockRateHz;
 extern uint32_t PLLClockRateHz;
 
-
-dma_init_t spi_rx_dma_config = SPI1_RXDMA_CONT_CONFIG(NULL, 2);
-dma_init_t spi_tx_dma_config = SPI1_TXDMA_CONT_CONFIG(NULL, 1);
-SPI_InitConfig_t spi_config = {
-    .data_len  = 8,
-    .nss_sw = true,
-    .nss_gpio_port = SPI_CS_PORT,
-    .nss_gpio_pin = SPI_CS_PIN,
-    .rx_dma_cfg = &spi_rx_dma_config,
-    .tx_dma_cfg = &spi_tx_dma_config,
-    .periph = SPI1
-};
-
-SPI_InitConfig_t spi_config_nonDMA = {
-    .data_len  = 8,
-    .nss_sw = true,
-    .nss_gpio_port = SPI_CS_PORT,
-    .nss_gpio_pin = SPI_CS_PIN,
-    .rx_dma_cfg = 0,
-    .tx_dma_cfg = 0,
-    .periph = SPI1
-};
-char msg[100];
-
-
 void HardFault_Handler();
-
 void ledblink();
-void testUsart();
+void ledoff();
+
+static void eth_init(void)
+{
+    uint32_t timeout_ms;
+    timeout_ms = tick_ms;
+    PHAL_writeGPIO(DAQ_ETH_RST_PORT, DAQ_ETH_RST_PIN, 0);
+    while (tick_ms - timeout_ms < ETH_PHY_RESET_PERIOD_MS);
+    PHAL_writeGPIO(DAQ_ETH_RST_PORT, DAQ_ETH_RST_PIN, 1);
+    timeout_ms = tick_ms;
+    while (tick_ms - timeout_ms < ETH_PHY_RESET_PERIOD_MS);
+}
 
 int main()
 {
@@ -120,82 +101,94 @@ int main()
     {
         HardFault_Handler();
     }
-    if (!PHAL_SPI_init(&spi_config))
+
+#if 0
+    PHAL_writeGPIO(DAQ_ETH_RST_PORT, DAQ_ETH_RST_PIN, 1);
+
+    if (!PHAL_SPI_init(&daq_spi1_config))
         HardFault_Handler();
-    if(!PHAL_initADC(ADC1, &adc_config, adc_channel_config, sizeof(adc_channel_config)/sizeof(ADCChannelConfig_t)))
+
+    SysTick_Config(SystemCoreClock / 1000);
+
+    /* Task Creation */
+    //schedInit(APB1ClockRateHz);
+
+    ledoff();
+    /* Schedule Periodic tasks here */
+    //taskCreate(ledblink, 250);
+    //schedStart();
+
+    PHAL_writeGPIO(DAQ_SPI1_NSS_PORT, DAQ_SPI1_NSS_PIN, 1);
+    eth_init();
+    PHAL_writeGPIO(DAQ_SPI1_NSS_PORT, DAQ_SPI1_NSS_PIN, 1);
+
+#define WHO_AM_I 0x8F
+#define I_AM_HIM 0x3F
+
+    PHAL_writeGPIO(DAQ_SPI1_NSS_PORT, DAQ_SPI1_NSS_PIN, 0);
+
+    uint32_t addr =  VERSIONR;
+    addr |= (_W5500_SPI_READ_ | _W5500_SPI_VDM_OP_);
+
+    uint8_t out_data[3] = {WHO_AM_I, 0, 0};
+    uint8_t in_data[3] = {0, 0, 0};
+    #if 0
+	in_data[0] = (addr & 0x00FF0000) >> 16;
+	in_data[1] = (addr & 0x0000FF00) >> 8;
+	in_data[2] = (addr & 0x000000FF) >> 0;
+    #endif
+
+    PHAL_SPI_transfer_noDMA(&daq_spi1_config, (uint8_t *)&out_data, 3, 3, in_data);
+
+    PHAL_writeGPIO(DAQ_SPI1_NSS_PORT, DAQ_SPI1_NSS_PIN, 1);
+    while (PHAL_SPI_busy(&daq_spi1_config))
+        ;
+
+    if (in_data[0] || in_data[1] || in_data[2])
     {
-        HardFault_Handler();
+        ledblink();
+        //ledoff();
+        //schedStart();
     }
-    if(!PHAL_initUSART(&lcd, APB1ClockRateHz))
+    else
     {
-        HardFault_Handler();
+        //ledoff();
+        //ledblink();
     }
-    if(!PHAL_initDMA(&adc_dma_config))
-    {
-        HardFault_Handler();
-    }
-    PHAL_writeGPIO(SPI_CS_PORT, SPI_CS_PIN, 1);
-    PHAL_startTxfer(&adc_dma_config);
-    PHAL_startADC(ADC1);
-    PHAL_usartRxDma(&lcd, (uint16_t *) msg, 5, 1);
-        /* Task Creation */
+#else
     schedInit(APB1ClockRateHz);
-        taskCreate(ledblink, 50);
-        taskCreate(testUsart, 100);
-        /* Schedule Periodic tasks here */
+    taskCreate(ledblink, 250);
     schedStart();
+#endif
+
     return 0;
 }
 
-
-void usart_recieve_complete_callback(usart_init_t *handle)
+void ledoff()
 {
-    if (handle == &lcd)
-    {
-        PHAL_toggleGPIO(GPIOD, 15);
-    }
-    else
-    {
-        PHAL_writeGPIO(GPIOD, 15, 0);
-    }
-}
+    PHAL_writeGPIO(GPIOA, 8, 0);
+    PHAL_writeGPIO(GPIOA, 9, 0);
+    PHAL_writeGPIO(GPIOA, 10, 0);
 
-void testUsart()
-{
-    char* txmsg = "Hello World!\n";
-    PHAL_usartTxDma(&lcd, (uint16_t *)txmsg, 13);
-    if (strcmp(msg, "hello") == 0)
-    {
-        PHAL_writeGPIO(GPIOD, 14, 1);
-    }
-    else
-    {
-        PHAL_writeGPIO(GPIOD, 14, 0);
-    }
+    PHAL_writeGPIO(GPIOD, 13, 0);
+    PHAL_writeGPIO(GPIOD, 14, 0);
+    PHAL_writeGPIO(GPIOD, 15, 0);
 }
 
 void ledblink()
 {
-    uint8_t out_data[3] = {WHO_AM_I, 0, 0};
-    uint8_t in_data[3] = {0};
-    // PHAL_SPI_transfer_noDMA(&spi_config, &out_data, 1, 1, in_data);
-    while (PHAL_SPI_busy(&spi_config))
-        ;
-    PHAL_SPI_transfer(&spi_config, out_data, 2, in_data);
-    while (PHAL_SPI_busy(&spi_config))
-        ;
+    PHAL_toggleGPIO(GPIOA, 8);
+    PHAL_toggleGPIO(GPIOA, 9);
+    PHAL_toggleGPIO(GPIOA, 10);
 
-    uint8_t in_data_nonDMA[3] = {0};
-    PHAL_SPI_transfer_noDMA(&spi_config_nonDMA, out_data, 1, 1, in_data_nonDMA);
-    if (in_data[1] == I_AM_HIM)
-        PHAL_writeGPIO(GPIOD, 13, 1);
-    else
-        PHAL_writeGPIO(GPIOD, 13, 0);
+    PHAL_toggleGPIO(GPIOD, 13);
+    PHAL_toggleGPIO(GPIOD, 14);
+    PHAL_toggleGPIO(GPIOD, 15);
+}
 
-    if (in_data_nonDMA[1] == I_AM_HIM)
-        PHAL_writeGPIO(GPIOD, 12, 1);
-    else
-        PHAL_writeGPIO(GPIOD, 12, 0);
+void SysTick_Handler(void)
+{
+    tick_ms++;
 }
 
 void HardFault_Handler()
