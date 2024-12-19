f297102538dfa8a7d6ab5a97e4357d7018f36fa6
Eileen Yoon <eyn@gmx.com>
2024-12-19 02:17:50 -0800

    main - shit

--- source/main_module/main.c
+++ source/main_module/main.c
@@ -494,7 +494,11 @@ void CAN1_RX0_IRQHandler()
 
 void main_module_bl_cmd_CALLBACK(CanParsedData_t *msg_data_a)
 {
-    if (can_data.main_module_bl_cmd.cmd == BLCMD_RST)
+    if (can_data.main_module_bl_cmd.cmd == 0x00)
+    {
+        PHAL_toggleGPIO(ERR_LED_GPIO_Port, ERR_LED_Pin);
+    }
+    else if (can_data.main_module_bl_cmd.cmd == BLCMD_RST)
         Bootloader_ResetForFirmwareDownload();
 }
 
