95e7f4b6bf01fd0e4a515870388e52025003428a
Eileen Yoon <eyn@gmx.com>
2025-05-31 14:23:08 -0700

    tmp

--- source/adbms/cells.c
+++ source/adbms/cells.c
@@ -89,6 +89,7 @@ static void bms_read_cells(void)
 static void bms_check_cells(void)
 {
     // TODO SIMD lol
+    // TODO Add LPF/EMA if needed
     bool set;
 
     #define CELL_REDUN_DELTA_MAX (0.05) // V
@@ -104,7 +105,64 @@ static void bms_check_cells(void)
     }
 }
 
+static inline uint32_t as_uint(const float32_t x) {
+    return *(uint32_t *)&x;
+}
+
+static inline float32_t as_float(const uint32_t x) {
+    return *(float32_t *)&x;
+}
+
+// https://stackoverflow.com/a/60047308
+static float32_t half_to_float(const uint16_t x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
+    const uint32_t e = (x&0x7C00)>>10; // exponent
+    const uint32_t m = (x&0x03FF)<<13; // mantissa
+    const uint32_t v = as_uint((float32_t)m)>>23; // evil log2 bit hack to count leading zeros in denormalized format
+    return as_float((x&0x8000)<<16 | (e!=0)*((e+112)<<23|m) | ((e==0)&(m!=0))*((v-37)<<23|((m<<(150-v))&0x007FE000))); // sign : normalized : denormalized
+}
+
+static uint16_t float_to_half(const float32_t x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
+    const uint32_t b = as_uint(x)+0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
+    const uint32_t e = (b&0x7F800000)>>23; // exponent
+    const uint32_t m = b&0x007FFFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
+    return (b&0x80000000)>>16 | (e>112)*((((e-112)<<10)&0x7C00)|m>>13) | ((e<113)&(e>101))*((((0x007FF000+m)>>(125-e))+1)>>1) | (e>143)*0x7FFF; // sign : normalized : denormalized : saturate
+}
+
+static void bms_process_cell_readings(void)
+{
+    // Calculate min/max/average
+
+    float mod_volts[TOTAL_AD68][4]; // min, max, avg, delta
+    for (int ic = 0; ic < TOTAL_AD68; ic++)
+    {
+        float min = data.cell_v_c[ic][0];
+        float max = data.cell_v_c[ic][0];
+        float avg = 0.0f;
+        for (int cell = 0; cell < TOTAL_CELL; cell++)
+        {
+            float volts = data.cell_v_c[ic][cell];
+            min = MIN(volts, min);
+            max = MAX(volts, max);
+            avg += volts;
+        }
+        avg /= (float)TOTAL_CELL;
+
+        mod_volts[ic][0] = min;
+        mod_volts[ic][1] = max;
+        mod_volts[ic][2] = avg;
+        mod_volts[ic][3] = max - min;
+    }
+}
+
 static void bms_send_cells(void)
 {
-    ;
+    // 1. Raw voltages. Send 16 bit
+    // raw_cell_voltage_a_0
+    // raw_cell_voltage_a_1
+    // raw_cell_voltage_a_2
+    // raw_cell_voltage_a_3
+    // ...
+    // raw_cell_voltage_f_3
+
+    // 2. min/max
 }
