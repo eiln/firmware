caacdf55917b9d6b0a2ccd9ab755524265b16baa
Eileen Yoon <eyn@gmx.com>
2025-05-31 17:53:19 -0700

    g4: rtos blink

--- source/g4_testing/main.c
+++ source/g4_testing/main.c
@@ -1,14 +1,9 @@
+
 #include "common/phal_G4/gpio/gpio.h"
 #include "common/phal_G4/rcc/rcc.h"
+#include "common/freertos/freertos.h"
 
-#define LED_GREEN_PORT  GPIOB
-#define LED_GREEN_PIN   7
-#define LED_RED_PORT    GPIOB
-#define LED_RED_PIN     5
-#define LED_BLUE_PORT   GPIOA
-#define LED_BLUE_PIN    15
-#define LED_ORANGE_PORT GPIOB
-#define LED_ORANGE_PIN  1
+#include "main.h"
 
 GPIOInitConfig_t gpio_config[] = {
     GPIO_INIT_OUTPUT(LED_GREEN_PORT, LED_GREEN_PIN, GPIO_OUTPUT_LOW_SPEED),
@@ -35,8 +30,20 @@ extern uint32_t PLLClockRateHz;
 
 void HardFault_Handler();
 
+static void ledblink1(void);
+static void ledblink2(void);
+static void ledblink3(void);
+static void ledblink4(void);
+
+defineThreadStack(ledblink1, 250, osPriorityNormal, 64);
+defineThreadStack(ledblink2, 300, osPriorityNormal, 64);
+defineThreadStack(ledblink3, 500, osPriorityNormal, 64);
+defineThreadStack(ledblink4, 1000, osPriorityNormal, 64);
+
 int main()
 {
+    osKernelInitialize();
+
     if (0 != PHAL_configureClockRates(&clock_config))
     {
         HardFault_Handler();
@@ -51,12 +58,41 @@ int main()
     PHAL_writeGPIO(LED_RED_PORT, LED_RED_PIN, 1);
     PHAL_writeGPIO(LED_BLUE_PORT, LED_BLUE_PIN, 1);
     PHAL_writeGPIO(LED_ORANGE_PORT, LED_ORANGE_PIN, 1);
+
+    // Create threads
+    createThread(ledblink1);
+    createThread(ledblink2);
+    createThread(ledblink3);
+    createThread(ledblink4);
+
+    osKernelStart(); // Go!
+
     while (1)
     {
         ;
     }
 
     return 0;
+}
+
+static void ledblink1(void)
+{
+    PHAL_toggleGPIO(LED_GREEN_PORT, LED_GREEN_PIN);
+}
+
+static void ledblink2(void)
+{
+    PHAL_toggleGPIO(LED_RED_PORT, LED_RED_PIN);
+}
+
+static void ledblink3(void)
+{
+    PHAL_toggleGPIO(LED_BLUE_PORT, LED_BLUE_PIN);
+}
+
+static void ledblink4(void)
+{
+    PHAL_toggleGPIO(LED_ORANGE_PORT, LED_ORANGE_PIN);
 }
 
 void HardFault_Handler()
---
+++ source/g4_testing/main.h
@@ -0,0 +1,14 @@
+
+#ifndef __G4_TESTING_MAIN_H__
+#define __G4_TESTING_MAIN_H__
+
+#define LED_GREEN_PORT  GPIOB
+#define LED_GREEN_PIN   7
+#define LED_RED_PORT    GPIOB
+#define LED_RED_PIN     5
+#define LED_BLUE_PORT   GPIOA
+#define LED_BLUE_PIN    15
+#define LED_ORANGE_PORT GPIOB
+#define LED_ORANGE_PIN  1
+
+#endif // __G4_TESTING_MAIN_H__
