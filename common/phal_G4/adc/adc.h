0a60d8e7959104725f5343c0f7a44fe137a2d0e0
Eileen Yoon <eyn@gmx.com>
2025-05-31 19:32:04 -0700

    tmp

--- common/phal_G4/adc/adc.h
+++ common/phal_G4/adc/adc.h
@@ -70,14 +70,14 @@ typedef struct {
     ADCChannelSampleCycles_t sampling_time;
 } ADCChannelConfig_t;
 
-// TODO DMA CONFIGS FOR ADC
-#define ADC1_DMA_CONT_CONFIG(periph, mem_addr_, tx_size_, priority_) \
-    {.periph_addr=(uint32_t) &(periph->DR), .mem_addr=mem_addr_,     \
+// TODO DMA CONFIGS FOR ADC2/3/4/5
+#define ADC1_DMA_CONT_CONFIG(mem_addr_, tx_size_, priority_)         \
+    {.periph_addr=(uint32_t) &(ADC1->DR), .mem_addr=mem_addr_,       \
      .tx_size=tx_size_, .increment=true, .circular=true,             \
      .dir=0b0, .mem_inc=true, .periph_inc=false, .mem_to_mem=false,  \
      .priority=priority_, .mem_size=0b01, .periph_size=0b01,         \
      .tx_isr_en=false, .dma_chan_request=0b0000, .stream_idx=0,      \
-     .periph=periph, .stream=DMA2_Stream0}
+     .periph=ADC1, .stream=DMA2_Stream0}
 
 /**
  * @brief Initializes the ADC, requires GPIO config prior
