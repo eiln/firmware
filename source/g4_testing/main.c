a7edcb6c37810db71ad93af5d9262c858b7a462c
Eileen Yoon <eyn@gmx.com>
2025-06-01 12:49:24 -0700

    tmp

--- source/g4_testing/main.c
+++ source/g4_testing/main.c
@@ -94,8 +94,9 @@ int main()
     {
         HardFault_Handler();
     }
-    PHAL_startTxfer(&adc_dma_config);
+    
     PHAL_startADC(&adc_config);
+    PHAL_startTxfer(&adc_dma_config);
 
     PHAL_writeGPIO(LED_GREEN_PORT, LED_GREEN_PIN, 1);
     PHAL_writeGPIO(LED_RED_PORT, LED_RED_PIN, 1);
