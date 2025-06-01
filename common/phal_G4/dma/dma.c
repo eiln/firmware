ae05469bdadf03f56208c75c0640ec1afc3727b5
Eileen Yoon <eyn@gmx.com>
2025-06-01 13:47:32 -0700

    tmp

--- common/phal_G4/dma/dma.c
+++ common/phal_G4/dma/dma.c
@@ -39,10 +39,9 @@ bool PHAL_initDMA(dma_init_t* dma) {
     // Clear any stream dedicated status flags that may have been set previously
     dma->periph->IFCR = (DMA_ISR_GIF1 << (dma->channel_idx & 0x1FU));
 
-    // Set peripheral port register address
+    // Set peripheral address (src)
     dma->channel->CPAR = dma->periph_addr;
-
-    // Set memory address
+    // Set memory address (dst)
     dma->channel->CMAR = dma->mem_addr;
     dma->channel->CNDTR = 1;
 
@@ -50,6 +49,7 @@ bool PHAL_initDMA(dma_init_t* dma) {
     dma->channel->CCR = 0;
     // Set channel, priority, memory data size
     dma->channel->CCR |= (dma->mem_size   << DMA_CCR_MSIZE_Pos) & DMA_CCR_MSIZE_Msk |
+    (dma->periph_size << DMA_CCR_PSIZE_Pos) & DMA_CCR_PSIZE_Msk |
                          (dma->priority   << DMA_CCR_PL_Pos) & DMA_CCR_PL_Msk   |
                          (dma->mem_inc    << DMA_CCR_MINC_Pos) & DMA_CCR_MINC_Msk |
                          (dma->periph_inc << DMA_CCR_PINC_Pos) & DMA_CCR_PINC_Msk |
@@ -57,12 +57,13 @@ bool PHAL_initDMA(dma_init_t* dma) {
                          (dma->dir        << DMA_CCR_DIR_Pos)  & DMA_CCR_DIR_Msk |
                          (dma->tx_isr_en  << DMA_CCR_TEIE_Pos) & DMA_CCR_TEIE_Msk |
                          (dma->tx_isr_en  << DMA_CCR_TCIE_Pos) & DMA_CCR_TCIE_Msk;
-
+#if 0
     DMA1_Channel1->CPAR = (uint32_t)&ADC1->DR;    // Peripheral address
     DMA1_Channel1->CCR = DMA_CCR_PL_1    // Priority medium
                        | DMA_CCR_MSIZE_0 | DMA_CCR_PSIZE_0  // 16-bit memory & peripheral size
                        | DMA_CCR_MINC    // Memory increment
                        | DMA_CCR_CIRC;    // Circular mode
+                       #endif
                        dma->channel->CCR |= DMA_CCR_EN;
 
                        RCC->AHB1ENR |= RCC_AHB1ENR_DMAMUX1EN;
